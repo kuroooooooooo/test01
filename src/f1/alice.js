a550b3d33777b60000527e2ed8ba3943d4b9bf5f
yejun <qkrdpwns200@naver.com>
2023-03-27 17:46:01 +0900

    Initial commit

---
+++ src/f1/alice.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.poem-stanza').addClass('highlight');
+});
+
+
+
+function emphasizePoemStanzas(){
+    $('.poem-stanza').addClass('highlight');
+}
+
+$(document).ready(highlightPoemStanzas);
---
+++ src/f2/jquerys.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#selected-plays > li').addClass('horizontal');
+    $('#selected-plays li:not(.horizontal)').addClass('sub-level');
+});
+
+
+$('img[alt]')
+
+$(document).ready(function(){
+    $('a[href^=mailto:]').addClass('mailto');
+    $('a[href$=.pdf]').addClass('pdflink');
+    $('a[href^=http][href*=henry').addClass('henrylink');
+});
